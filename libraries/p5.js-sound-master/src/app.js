26f0556f37bc92adc70e2f6b4ab10bcb7ab13620
rheugle <rheugle@rheugle-macbookpro2.roam.corp.google.com>
2016-01-14 20:51:52 -0800

    imitation art version 1

---
+++ libraries/p5.js-sound-master/src/app.js
@@ -0,0 +1,30 @@
+define(function (require) {
+
+  'use strict';
+
+  var p5SOUND = require('sndcore');
+  require('master');
+  require('helpers');
+  require('errorHandler');
+  require('panner');
+  require('soundfile');
+  require('amplitude');
+  require('fft');
+  require('signal');
+  require('oscillator');
+  require('env');
+  require('pulse');
+  require('noise');
+  require('audioin');
+  require('filter');
+  require('delay');
+  require('reverb');
+  require('metro');
+  require('looper');
+  require('soundRecorder');
+  require('peakdetect');
+  require('gain');
+
+  return p5SOUND;
+
+});
---
+++ sketch.js
@@ -0,0 +1,72 @@
+// // Learning Processing
+// // Daniel Shiffman
+// // http://www.learningprocessing.com
+// // Example 1-1: stroke and fill
+
+//Sound Components
+var osc;
+
+var particles = [];
+var N = 400;
+
+function setup() {
+
+  createCanvas(windowWidth, windowHeight);
+  noStroke(); 
+  background(255);
+
+  for (var i=0;i<N;i++){
+    particles.push(new particle());
+  }
+
+  for (var i=0;i<N;i++){
+    particles[i].oscillate();
+  }
+
+}
+
+function draw(){
+  for (var i=0;i<N;i++){
+    particles[i].move();
+    particles[i].display(); 
+  }
+  background(255,255,255,20);
+}
+
+function particle(){
+  
+  //console.log(this);
+  this.distance = random(20,320);
+  this.radius = random(2,30);
+  this.theta = 0;
+  this.x = 0;
+  this.y = 0;
+  this.inc = random(-0.02,0.02);
+  this.frequency = this.distance/(320)*1000;
+
+  this.r = random(50,200);
+  this.g = random(200,220);
+  this.b = random(0);
+  this.alph = random(0,255);
+
+  this.move = function(){
+     this.theta += this.inc;
+     this.x = windowWidth/2+this.distance*cos(this.theta);
+     this.y = windowHeight/2+this.distance*sin(this.theta);
+     console.log()
+  }
+
+  this.display = function(){
+    fill(this.r,this.g,this.b,this.alph);
+    ellipse(this.x,this.y,this.radius,this.radius);
+  }
+
+  this.oscillate = function(){
+    this.osc = new p5.Oscillator();
+    this.osc.setType('sine');
+    this.osc.freq(this.frequency);
+    this.osc.amp(2.0);
+    this.osc.start();
+  }
+  
+}
